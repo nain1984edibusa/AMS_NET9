ebc3aa473ba5048c24d2ec49984f16935a3d6897
Eduardo Bustillos <edibusa_1991@hotmail.com>
2025-06-21 23:09:04 -0500

    Avance 8

--- App/Services/Microservices.Demo.Report.Service/DTO/PolicyDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Microservices.Demo.Report.Service.DTO
-{
-    public class PolicyDto
-    {
-        public int Id { get; set; }
-        public string Holder { get; set; }
-        public int ProductId { get; set; }
-    }
-}
--- App/Services/Microservices.Demo.Report.Service/DTO/ProductDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Microservices.Demo.Report.Service.DTO
-{
-    public class ProductDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Coverage { get; set; }
-    }
-}

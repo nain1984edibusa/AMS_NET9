afb3e206d804e11f274cecce721559c5d8edb2f5
Eduardo Bustillos <edibusa_1991@hotmail.com>
2025-06-25 13:59:08 -0500

    app

--- App/Clients/ng-microservices-demo-client/src/app/ui/insurance/reports/pages/reports-list/reports-list.ts
+++ App/Clients/ng-microservices-demo-client/src/app/ui/insurance/reports/pages/reports-list/reports-list.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-reports-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatTableModule],
   templateUrl: './reports-list.html',
-  styleUrl: './reports-list.scss'
 })
 export class ReportsList {
+  displayedColumns: string[] = ['cedula', 'apellidos', 'nombres'];
 
+  dataSource = [
+    { cedula: '0102030405', apellidos: 'Bustillos Tapia', nombres: 'Eduardo Andrés' },
+    { cedula: '0607080910', apellidos: 'Tapia Cueva', nombres: 'Freddy Rolando' },
+    { cedula: '1101121314', apellidos: 'García León', nombres: 'María Elena' }
+  ];
 }
